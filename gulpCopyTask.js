e78d02e7fb5f16caa403c3bd2bbe3b35e8edcee8
Violetta <violett.vitacat@gmail.com>
2023-06-28 18:10:49 +0300

    asda

--- gulpCopyTask.js
+++ gulpCopyTask.js
@@ -6,7 +6,5 @@ const copyScss=()=>{
     return src(['src/**/*.scss', '!src/pages/testFolder/**'])
         .pipe(dest('build/styles'))
 }
-exports.copyScss=copyScss;
 
-gulp.task('default', copyScss)
-
+exports.copy=copyScss;
