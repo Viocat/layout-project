1e28a0a8c32197441b476c4eb4d8818f8ec82d38
Violetta <violett.vitacat@gmail.com>
2023-06-28 20:54:30 +0300

    added readme

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,22 @@
 const gulp = require('gulp');
-const {src, dest, parallel } =require('gulp');
+const {src, dest, parallel} =require('gulp');
 const copyScss=()=>{
-    // return src('src/scss/**/*.scss')
-    // .pipe(dest('build/styles'))
     return src(['src/**/*.scss', '!src/pages/testFolder/**'])
         .pipe(dest('build/styles'))
 }
 
-// const copyTxt=()=>{
-//     return src('src/**/*.txt')
-//         .pipe(dest('build/txt-files'))
-// }
-//
-// const copyHtml=()=>{
-//     return src('[src/pages/**/*.html]', '!src/pages/testFolder/**')
-//         .pipe(dest('src/pages'))
-// }
+const copyTxt=()=>{
+    return src('src/**/*.txt')
+        .pipe(dest('build/txt-files'))
+}
 
-exports.lala=copyScss; // позволяет вызвать такс copyScss, прописывая прописывая gulp lala
-// exports.fullCopy=parallel(copyTxt, copyScss,copyHtml)
-gulp.task('default',lala) //функция запускается по дефолту при вызове gulp в консоли
+const copyHtml=()=>{
+    return src(['src/pages/**/*.html', '!src/pages/testFolder/**'])
+        .pipe(dest('build/pages'))
+}
+
+exports.scssCopy=copyScss; // позволяет вызвать такс copyScss, прописывая прописывая gulp cssCopy и тд
+exports.txtCopy=copyTxt;
+exports.htmlCopy=copyHtml;
+gulp.task('default',copyScss) //функция запускается по дефолту при вызове gulp в консоли
 
